09a856ed70242672a9f5586de3c644f783216c02
uifehnah <85141036+uifehnah@users.noreply.github.com>
2021-07-01 16:50:56 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,117 @@
-$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
+jQuery(document).ready(function($){
+    var dragging = false,
+        scrolling = false,
+        resizing = false;
+    //cache jQuery objects
+    var imageComparisonContainers = $('.cd-image-container');
+    //check if the .cd-image-container is in the viewport 
+    //if yes, animate it
+    checkPosition(imageComparisonContainers);
+    $(window).on('scroll', function(){
+        if( !scrolling) {
+            scrolling =  true;
+            ( !window.requestAnimationFrame )
+                ? setTimeout(function(){checkPosition(imageComparisonContainers);}, 100)
+                : requestAnimationFrame(function(){checkPosition(imageComparisonContainers);});
+        }
+    });
+    
+    //make the .cd-handle element draggable and modify .cd-resize-img width according to its position
+    imageComparisonContainers.each(function(){
+        var actual = $(this);
+        drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual, actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-image-label[data-type="modified"]'));
+    });
+
+    //upadate images label visibility
+    $(window).on('resize', function(){
+        if( !resizing) {
+            resizing =  true;
+            ( !window.requestAnimationFrame )
+                ? setTimeout(function(){checkLabel(imageComparisonContainers);}, 100)
+                : requestAnimationFrame(function(){checkLabel(imageComparisonContainers);});
+        }
+    });
+
+    function checkPosition(container) {
+        container.each(function(){
+            var actualContainer = $(this);
+            if( $(window).scrollTop() + $(window).height()*0.5 > actualContainer.offset().top) {
+                actualContainer.addClass('is-visible');
+            }
+        });
+
+        scrolling = false;
+    }
+
+    function checkLabel(container) {
+        container.each(function(){
+            var actual = $(this);
+            updateLabel(actual.find('.cd-image-label[data-type="modified"]'), actual.find('.cd-resize-img'), 'left');
+            updateLabel(actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-resize-img'), 'right');
+        });
+
+        resizing = false;
+    }
+
+    //draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
+    function drags(dragElement, resizeElement, container, labelContainer, labelResizeElement) {
+        dragElement.on("mousedown vmousedown", function(e) {
+            dragElement.addClass('draggable');
+            resizeElement.addClass('resizable');
+
+            var dragWidth = dragElement.outerWidth(),
+                xPosition = dragElement.offset().left + dragWidth - e.pageX,
+                containerOffset = container.offset().left,
+                containerWidth = container.outerWidth(),
+                minLeft = containerOffset + 10,
+                maxLeft = containerOffset + containerWidth - dragWidth - 10;
+            
+            dragElement.parents().on("mousemove vmousemove", function(e) {
+                if( !dragging) {
+                    dragging =  true;
+                    ( !window.requestAnimationFrame )
+                        ? setTimeout(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);}, 100)
+                        : requestAnimationFrame(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);});
+                }
+            }).on("mouseup vmouseup", function(e){
+                dragElement.removeClass('draggable');
+                resizeElement.removeClass('resizable');
+            });
+            e.preventDefault();
+        }).on("mouseup vmouseup", function(e) {
+            dragElement.removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+    }
+
+    function animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement) {
+        var leftValue = e.pageX + xPosition - dragWidth;   
+        //constrain the draggable element to move inside his container
+        if(leftValue < minLeft ) {
+            leftValue = minLeft;
+        } else if ( leftValue > maxLeft) {
+            leftValue = maxLeft;
+        }
+
+        var widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+        
+        $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
+            $(this).removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+
+        $('.resizable').css('width', widthValue); 
+
+        updateLabel(labelResizeElement, resizeElement, 'left');
+        updateLabel(labelContainer, resizeElement, 'right');
+        dragging =  false;
+    }
+
+    function updateLabel(label, resizeElement, position) {
+        if(position == 'left') {
+            ( label.offset().left + label.outerWidth() < resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
+        } else {
+            ( label.offset().left > resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
+        }
+    }
 });
-function initComparisons() {
-	var x, i;
-	/*find all elements with an "overlay" class:*/
-	x = document.getElementsByClassName("img-comp-overlay");
-	for (i = 0; i < x.length; i++) {
-	  /*once for each "overlay" element:
-	  pass the "overlay" element as a parameter when executing the compareImages function:*/
-	  compareImages(x[i]);
-	}
-	function compareImages(img) {
-	  var slider, img, clicked = 0, w, h;
-	  /*get the width and height of the img element*/
-	  w = img.offsetWidth;
-	  h = img.offsetHeight;
-	  /*set the width of the img element to 50%:*/
-	  img.style.width = (w / 2) + "px";
-	  /*create slider:*/
-	  slider = document.createElement("DIV");
-	  slider.setAttribute("class", "img-comp-slider");
-	  /*insert slider*/
-	  img.parentElement.insertBefore(slider, img);
-	  /*position the slider in the middle:*/
-	  slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-	  slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
-	  /*execute a function when the mouse button is pressed:*/
-	  slider.addEventListener("mousedown", slideReady);
-	  /*and another function when the mouse button is released:*/
-	  window.addEventListener("mouseup", slideFinish);
-	  /*or touched (for touch screens:*/
-	  slider.addEventListener("touchstart", slideReady);
-	  /*and released (for touch screens:*/
-	  window.addEventListener("touchend", slideFinish);
-	  function slideReady(e) {
-		/*prevent any other actions that may occur when moving over the image:*/
-		e.preventDefault();
-		/*the slider is now clicked and ready to move:*/
-		clicked = 1;
-		/*execute a function when the slider is moved:*/
-		window.addEventListener("mousemove", slideMove);
-		window.addEventListener("touchmove", slideMove);
-	  }
-	  function slideFinish() {
-		/*the slider is no longer clicked:*/
-		clicked = 0;
-	  }
-	  function slideMove(e) {
-		var pos;
-		/*if the slider is no longer clicked, exit this function:*/
-		if (clicked == 0) return false;
-		/*get the cursor's x position:*/
-		pos = getCursorPos(e)
-		/*prevent the slider from being positioned outside the image:*/
-		if (pos < 0) pos = 0;
-		if (pos > w) pos = w;
-		/*execute a function that will resize the overlay image according to the cursor:*/
-		slide(pos);
-	  }
-	  function getCursorPos(e) {
-		var a, x = 0;
-		e = e || window.event;
-		/*get the x positions of the image:*/
-		a = img.getBoundingClientRect();
-		/*calculate the cursor's x coordinate, relative to the image:*/
-		x = e.pageX - a.left;
-		/*consider any page scrolling:*/
-		x = x - window.pageXOffset;
-		return x;
-	  }
-	  function slide(x) {
-		/*resize the image:*/
-		img.style.width = x + "px";
-		/*position the slider:*/
-		slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
-	  }
-	}
-  }
